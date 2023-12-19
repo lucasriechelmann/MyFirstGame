0cd94d60677687e131e5abeb17b39e8dd48141ac
Lucas Riechelmann Ramos <lucas.riechelmann@hotmail.com>
2023-12-19 08:18:46 +0000

    added

---
+++ MyFirstGame/Engine/Objects/IGameObjectPool.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyFirstGame.Engine.Objects;
+
+public interface IGameObjectPool<T> where T : BaseGameObject
+{
+    List<T> ActiveObjects { get; }
+
+    T GetOrCreate(Func<T> createNbObjectFn);
+
+    void DeactivateObject(T gameObject, Action<T> postDeactivateFn);
+    void DeactivateAllObjects(Action<T> postDeactivateFn);
+    void DeactivateAllObjects();
+}
